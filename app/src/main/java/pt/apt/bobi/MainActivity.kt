8e4686a2fdc10270b68d2f8aaf8deb1c04f1af13
hyobu <luis.arez.antunes@gmail.com>
2021-04-12 02:50:57 +0100

    v1

--- app/src/main/java/pt/apt/bobi/MainActivity.kt
+++ app/src/main/java/pt/apt/bobi/MainActivity.kt
@@ -1,11 +1,35 @@
 package pt.apt.bobi
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
+private  const val REQUEST_IMAGE_CAPTURE = 100
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            openNativeCamera()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+    private fun openNativeCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
     }
 }
